20ee4fe0fd9f86bcfec7b653cdf9d96dd756a9c6
panutat-p <pp.panutat@gmail.com>
2023-03-07 00:06:11 +0700

    feat: add AppHeader

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import AppHeader from './components/AppHeader'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
+        <AppHeader></AppHeader>
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AppHeader() {
+    return (
+        <h1>Hello 😁</h1>
+    );
+}
+
+export default AppHeader;
