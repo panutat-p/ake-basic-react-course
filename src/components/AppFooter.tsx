671be4ece8e65effba15bb96f41abf9c71cba865
panutat-p <pp.panutat@gmail.com>
2023-03-07 11:45:31 +0700

    feat: use React hook with custom type

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -1,19 +1,28 @@
 import React, { useState } from 'react';
 
+type User = {
+  name: string;
+};
+
 const AppFooter = () => {
   const [vite, setVite] = useState('Vite');
+  const [user, setUser] = useState<User>({ name: '😃' });
 
   function update() {
     setVite('Vite'.toUpperCase());
   }
 
+  setTimeout(() => {
+    setUser({ name: '😛' });
+  }, 2000);
+
   return (
     <>
-      <div>
-        <button onClick={update}>uppercase</button>
-      </div>
+      <h3>User: {user?.name}</h3>
       <p>Powered by React 18</p>
-      <p>Built by {vite}</p>
+      <p>
+        Built by {vite} <button onClick={update}>uppercase</button>
+      </p>
     </>
   );
 };
