4df1e94cdf337845956d4d3f47d0297a31e74ccb
panutat-p <pp.panutat@gmail.com>
2023-03-07 11:12:56 +0700

    feat: button onClick

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
 function AppHeader() {
-    return (
-        <h1>Hello 😁</h1>
-    );
+  function showAlert() {
+    alert('You clicked a button');
+  }
+
+  return (
+    <>
+      <div>
+        <button onClick={showAlert}>Click Me</button>
+      </div>
+      <h1>Hello 😁</h1>;
+    </>
+  );
 }
 
 export default AppHeader;
